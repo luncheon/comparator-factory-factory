6b0a737ff9fe93fefc1c69346162655dafae7539
luncheon <takeuck+github@gmail.com>
2018-04-23 15:20:59 +0900

    refactor

--- index.ts
+++ index.ts
@@ -1,18 +1,28 @@
-export interface OrderDefinition<T> {
-  readonly key?:      KeyDefinition<T> | KeyDefinition<T>[]
-  readonly desc?:     boolean
-  readonly nulls?:    NullHandling
-  readonly locales?:  string | string[]
-  readonly collator?: Intl.Collator | Intl.CollatorOptions
+export interface ComparisonRule<T> {
+  readonly key?:              ComparisonKey<T> | ComparisonKey<T>[]
+  readonly keyValueSelector?: ComparisonKeyValueSelector<T>
+  readonly desc?:             boolean
+  readonly nulls?:            NullsHandling
+  readonly locales?:          string | string[]
+  readonly collator?:         Intl.CollatorOptions & { compare?(a: string, b: string): number }
 }
-export type KeyDefinition<T>  = string | number | KeySelector<T>
-export type KeySelector<T>    = (element: T) => any | undefined
-export type NullHandling      = 'first' | 'last' | 'min' | 'max'
-export type Comparator<T>     = (a: T, b: T) => number
+export type ComparisonKey<T>              = string | number | ComparisonKeySelector<T>
+export type ComparisonKeySelector<T>      = (obj: T) => any | undefined
+export type ComparisonKeyValueSelector<T> = (key?: ComparisonKey<T>) => (obj: T) => any
+export type NullsHandling                 = 'first' | 'last' | 'min' | 'max'
+export type Comparator<T>                 = (a: T, b: T) => number
 
-const defaultCollator = new Intl.Collator()
+const defaultCollator = Intl.Collator()
 
-export default function comparing<T>(...orders: (OrderDefinition<T> | KeyDefinition<T>)[]): Comparator<any>
+function defaultKeyValueSelector<T>(key?: ComparisonKey<T>): (obj: T) => number {
+  return (
+    key === undefined || key === null || key === '' ? (term: T)   => term:
+    typeof key === 'function'                       ? (term: T)   => { try { return key(term) } catch {} } :
+                                                      (term: any) => { try { return term[key] } catch {} }
+  )
+}
+
+export default function comparing<T>(...rules: (ComparisonRule<T> | ComparisonKey<T>)[]): Comparator<any>
 export default function comparing<T>() {
   const comparators = createComparators<T>(arguments)
   switch (comparators.length) {
@@ -28,57 +38,50 @@ export default function comparing<T>() {
   }
 }
 
-function createComparators<T>(orders: ArrayLike<OrderDefinition<T> | KeyDefinition<T>>): Comparator<T>[] {
+function createComparators<T>(rules: ArrayLike<ComparisonRule<T> | ComparisonKey<T>>): Comparator<T>[] {
   const comparators: Comparator<T>[] = []
-  let order: OrderDefinition<T> | KeyDefinition<T>
-  for (let i = 0; i < orders.length; ++i) {
-    order = orders[i]
-    if (typeof order === 'object' && order) {
-      for (const key of Array.isArray(order.key) ? order.key : [order.key]) {
-        comparators.push(createComparator(key, order.desc, order.nulls, order.locales, order.collator))
+  let rule: ComparisonRule<T> | ComparisonKey<T>
+  for (let i = 0; i < rules.length; ++i) {
+    rule = rules[i]
+    if (typeof rule === 'object' && rule) {
+      for (const key of Array.isArray(rule.key) ? rule.key : [rule.key]) {
+        comparators.push(createComparator(key, rule))
       }
     } else {
-      comparators.push(createComparator(order))
+      comparators.push(createComparator(rule))
     }
   }
   return comparators
 }
 
-function createComparator<T>(
-  key?:             KeyDefinition<T>,
-  desc?:            boolean,
-  nulls?:           NullHandling,
-  locales?:         string | string[],
-  collatorOptions?: Intl.Collator | Intl.CollatorOptions
-): Comparator<T> {
-  const collator =
-    collatorOptions instanceof Intl.Collator  ? collatorOptions :
-    locales || collatorOptions                ? new Intl.Collator(locales, collatorOptions) :
-                                                defaultCollator
-  const keySelector =
-    key === undefined || key === null || key === '' ? (term: T)   => term:
-    typeof key === 'function'                       ? (term: T)   => { try { return key(term) } catch {} } :
-                                                      (term: any) => { try { return term[key] } catch {} }
-  const direction = desc ? -1 : 1
+function createComparator<T>(key?: ComparisonKey<T>, rule?: ComparisonRule<T>): Comparator<T> {
+  rule = rule || {}
+  const { nulls, locales, collator } = rule
+  const stringComparator =
+    collator && collator.compare ?  collator as { compare(a: string, b: string): number } :
+    locales || collator          ?  Intl.Collator(locales, collator) :
+                                    defaultCollator
+  const direction = rule.desc ? -1 : 1
   const nullsResult =
     nulls === 'first' ? -direction :
     nulls === 'last'  ?  direction :
     nulls === 'max'   ?  1 :
                         -1
-  function compare(a: any, b: any): number {
+  const keyValueSelector = (rule.keyValueSelector || defaultKeyValueSelector)(key)
+  function compareValue(a: any | null | undefined, b: any | null | undefined): number {
     return (
       a === b         ? 0 :
       a === undefined ?  nullsResult :
       b === undefined ? -nullsResult :
       a === null      ?  nullsResult :
       b === null      ? -nullsResult :
-      Array.isArray(a) && Array.isArray(b) ? compareArray(a, b, compare) :
-      typeof a === 'string' && typeof b === 'string' ? collator.compare(a, b) :
+      Array.isArray(a) && Array.isArray(b) ? compareArray(a, b, compareValue) :
+      typeof a === 'string' && typeof b === 'string' ? stringComparator.compare(a, b) :
       (a < b ? -1 : 1)
     )
   }
   return (a: T, b: T) => {
-    const result = compare(keySelector(a), keySelector(b))
+    const result = compareValue(keyValueSelector(a), keyValueSelector(b))
     return result && result * direction
   }
 }
